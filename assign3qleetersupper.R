dd7c35f30703a9d49eab81448bd4745e7bce5e1e
Saikumar7153 <114389272+Saikumar7153@users.noreply.github.com>
2023-06-19 18:23:44 +0530

    Create assign3qleetersupper.R

---
+++ assign3qleetersupper.R
@@ -0,0 +1,11 @@
+# Extract first 10 letters in lowercase
+first_10_lower <- letters[1:10]
+cat("First 10 letters in lowercase: ", first_10_lower, "\n")
+
+# Extract last 10 letters in uppercase
+last_10_upper <- toupper(letters[17:26])
+cat("Last 10 letters in uppercase: ", last_10_upper, "\n")
+
+# Extract letters between 22nd and 24th in uppercase
+middle_upper <- toupper(letters[22:24])
+cat("Letters between 22nd and 24th in uppercase: ", middle_upper, "\n")
